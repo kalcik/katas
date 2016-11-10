b8022e8184bfee80020340a9527eb36536b5b10b
Anton Kalcik <anton.kalcik@gmail.com>
2016-11-10 23:25:53 +0100

    Remove some classes to make Kata more straightforward Remove difficulty section from Reaadme.md

--- src/CarPlantKata/Car/Parts/Interior/InteriorColor.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Car.Parts.Interior
-{
-    public enum InteriorColor
-    {
-        BlackAlcantaraHeadliner = 0,
-        DarkAshWoodDecor = 1,
-        TanNextGenerationSeats = 2
-    }
-}
